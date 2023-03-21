34ae147fdfc3be647496fff670d87f0f615adec2
J-Pedr0 <jpnolasco70@gmail.com>
2023-03-20 21:49:56 -0300

    iniciando projeto

---
+++ agendamento/src/test/java/br/retr0/jp/agendamento/AgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.retr0.jp.agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
